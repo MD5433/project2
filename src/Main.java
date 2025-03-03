c0ed9deeee7037636da11a5781d281f914f2e3d7
Admin <mariahdungey01@gmail.com>
2025-03-03 01:16:27 -0500

    wk 7

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,49 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true) {
+            displayMenu();
+            try {
+                int choice = Integer.parseInt(Task.myIn.nextLine());
+                switch (choice) {
+                    case 1:
+                        Task.addTask();
+                        break;
+                    case 2:
+                        Task.removeTask();
+                        break;
+                    case 3:
+                        Task.editTask();
+                        break;
+                    case 4:
+                        Task.listTask();
+                        break;
+                    case 5:
+                        Task.listByPriority();
+                        break;
+                    case 0:
+                        System.out.println("Exiting program.");
+                        return;
+                    default:
+                        System.out.println("Invalid choice, please try again.");
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number.");
+            }
         }
+    }
+
+
+
+
+    static void displayMenu() {
+        System.out.println("Choose an option:");
+        System.out.println("(1) Add a task");
+        System.out.println("(2) Remove a task");
+        System.out.println("(3) Edit a task");
+        System.out.println("(4) List all tasks");
+        System.out.println("(5) List task by priority");
+        System.out.println("(0) Exit");
     }
 }
